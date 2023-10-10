20a55fc00330d05b7c93c47e92ef6e185deb5aee
Eugene-Lek <58066799+Eugene-Lek@users.noreply.github.com>
2023-10-10 19:43:47 +0800

    new: tests

---
+++ main_test.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"encoding/json"
+	"bytes"
+	"onecv-go-backend/models"
+	"testing"
+	"log"
+	"fmt"
+	"strings"
+	"context"
+	"regexp"
+
+	"github.com/pashagolub/pgxmock/v3"
+	"github.com/gin-gonic/gin"
+)
+
+type registerStudentsTestCase struct {
+	testCaseDesc string
+	body  models.StudentRegistrationData
+	emailsExist models.StudentRegistrationData
+	wantCode int
+	wantMessage string
+}
+
+func OneRegisterStudentTest(t *testing.T, router *gin.Engine, testCase registerStudentsTestCase) {
+	// First, add expected queries and results to the mock DB
+	teacher := testCase.body.Teacher
+	students := testCase.body.Students
+
+	mock, err := pgxmock.NewConn()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mock.Close(context.Background())
+
+	noRequestErrors := true
+
+	allEmails := append(students, teacher)
+	invalidEmails := getInvalidEmails(allEmails)
+	if len(invalidEmails) > 0 { 
+		noRequestErrors = false 
+	} else {
+		addCheckTeacherExistsQuery(mock, teacher, testCase.emailsExist.Teacher)
+		addCheckStudentExistsQueries(mock, students, testCase.emailsExist.Students)
+	}
+
+	allEmailsExistence := append(testCase.emailsExist.Students, testCase.emailsExist.Teacher)
+	for _, emailExist := range allEmailsExistence {
+		if emailExist == "false" { noRequestErrors = false }
+	}
+
+	if noRequestErrors {
+		for _, student := range students {
+			mock.ExpectQuery(regexp.QuoteMeta("INSERT INTO teacher_student_relationship(teacher, student) VALUES ($1, $2)")).WithArgs(teacher, student).WillReturnRows(pgxmock.NewRows([]string{"id", "teacher", "student"}))
+		}
+	}
+
+	models.DB = mock // assign the mock connection's pointer to models.DB so it can be used by the API endpoints
+
+	// Now, we make the API call
+    out, err := json.Marshal(testCase.body)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("POST", "/api/register", bytes.NewBuffer(out))
+	if err != nil {
+		t.Fatalf("building request: %v", err)
+	}
+
+	response := getResponse(request, router, recorder, t)
+
+	// make sure that all expectations were met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}	
+
+	if response.Status != testCase.wantCode {
+		t.Errorf("wrong response code:\nwant: %v\n got: %v", testCase.wantCode, response.Status)
+	}
+	if response.Message != testCase.wantMessage {
+		t.Errorf("wrong response message:\nwant: %s\n got: %s", testCase.wantMessage, response.Message)
+	}
+}
+
+func TestRegisterStudents(t *testing.T) {
+	testCases := []registerStudentsTestCase{
+        {
+			"All valid and existent emails", 
+			models.StudentRegistrationData{Teacher: "tom@gmail.com", Students: []string{"jerry@gmail.com", "spike@gmail.com"}},
+			models.StudentRegistrationData{Teacher: "true", Students: []string{"true", "true"}},
+			204,
+			"",
+		},
+        {
+			"One or more invalid emails", 
+			models.StudentRegistrationData{Teacher: "tomgmail.com", Students: []string{"jerry@gmailcom", "spike@gmail.com"}},
+			models.StudentRegistrationData{Teacher: "true", Students: []string{"true", "true"}},
+			400,
+			fmt.Sprintf(errorMessages["invalidEmail"], strings.Join([]string{"jerry@gmailcom", "tomgmail.com"}, ", ")),
+		},
+        {
+			"One or more invalid emails & non-existent email(s)", 
+			models.StudentRegistrationData{Teacher: "tomgmail.com", Students: []string{"jerry@gmailcom", "spike@gmail.com"}},
+			models.StudentRegistrationData{Teacher: "false", Students: []string{"true", "true"}},
+			400,
+			fmt.Sprintf(errorMessages["invalidEmail"], strings.Join([]string{"jerry@gmailcom", "tomgmail.com"}, ", ")),
+		},		
+		{
+			"Non existent teacher email", 
+			models.StudentRegistrationData{Teacher: "tom@gmail.com", Students: []string{"jerry@gmail.com", "spike@gmail.com"}},
+			models.StudentRegistrationData{Teacher: "false", Students: []string{"true", "true"}},
+			400,
+			fmt.Sprintf(models.ErrorMessages["nonExistentTeacher"], "tom@gmail.com"),
+		},
+		{
+			"Non existent student emails", 
+			models.StudentRegistrationData{Teacher: "tom@gmail.com", Students: []string{"jerry@gmail.com", "spike@gmail.com"}},
+			models.StudentRegistrationData{Teacher: "true", Students: []string{"false", "false"}},
+			400,
+			fmt.Sprintf(models.ErrorMessages["nonExistentStudents"], strings.Join([]string{"'jerry@gmail.com'", "'spike@gmail.com'"}, ", ")),
+		},	
+		{
+			"Non existent student & teacher emails", 
+			models.StudentRegistrationData{Teacher: "tom@gmail.com", Students: []string{"jerry@gmail.com", "spike@gmail.com"}},
+			models.StudentRegistrationData{Teacher: "false", Students: []string{"false", "true"}},
+			400,
+			fmt.Sprintf(models.ErrorMessages["nonExistentTeacher&Students"], "tom@gmail.com", strings.Join([]string{"'jerry@gmail.com'"}, ", ")),
+		},	
+
+		
+    }
+
+	router := router()
+	for _, tc := range testCases {
+		t.Run(tc.testCaseDesc, func(t *testing.T) {
+			OneRegisterStudentTest(t, router, tc)
+		})
+	}
+}
---
+++ main_test_helpers.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"regexp"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pashagolub/pgxmock/v3"
+)
+
+type Response struct {
+	Status int `json:"status" binding:"required"`
+	Message string `json:"message"`
+}
+
+type ResponseBody struct {
+	Message string `json:"message"`
+}
+
+func getResponse(request *http.Request, router *gin.Engine, recorder *httptest.ResponseRecorder, t *testing.T) Response {
+
+	router.ServeHTTP(recorder, request)
+
+	var responseBody ResponseBody
+	body := recorder.Body.String()
+	
+	if recorder.Code < 300 {
+		responseBody = ResponseBody{""}
+	} else {
+		if err := json.Unmarshal([]byte(body), &responseBody); err != nil {
+			t.Fatalf("parsing json response: %v", err)
+		}
+	}
+
+	return Response {
+		Status: recorder.Code,
+		Message: responseBody.Message,
+	}
+}
+
+
+func addCheckTeacherExistsQuery(mock pgxmock.PgxConnIface, teacher string, teacherExists string) {
+	expectedTeacherRow := pgxmock.NewRows([]string{"email"})
+	if (teacherExists == "true") {expectedTeacherRow.AddRow(teacher)}
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT email FROM teacher WHERE email = $1")).WithArgs(teacher).WillReturnRows(expectedTeacherRow)	
+}
+
+func addCheckStudentExistsQueries(mock pgxmock.PgxConnIface, students []string, studentExistences []string) {
+	for index, student := range students {
+		expectedStudentRow := pgxmock.NewRows([]string{"email"})
+		if (studentExistences[index] == "true") {
+			expectedStudentRow.AddRow(student)
+		}
+		
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT email FROM student WHERE email = $1")).WithArgs(student).WillReturnRows(expectedStudentRow)
+	}	
+}
